8b888c0ccc2df28ebb3492be89b005debbb8e30e
Tony Xu <tx@Tonys-MacBook-Pro.local>
2019-03-05 17:06:01 +1100

    Fixed Menus, Creating Percentage

--- source.rb
+++ source.rb
@@ -1,15 +1,53 @@
 require 'faker'
-<<<<<<< HEAD
 require 'colorize'
 
-def menu
-    puts "#{"*".light_blue}#{"*".cyan}#{"*".blue}"*17
-  puts "1. START GAME". center(50).light_blue
-  puts "2. EXIT".center(50).light_blue
-  puts "#{"*".light_blue}#{"*".cyan}#{"*".blue}"*17
-  input = gets.strip.to_i
+  
+def launch_screen
+  #ASKS WHETHER YOU WANT TO PLAY IN COLOURBLIND MODE
+  puts "*"*51
+  puts "COLOURBLIND MODE? (Y/N)".center(51)
+  puts "*"*51
+  input = gets().strip.upcase
 end
-
+  
+def main_menu 
+  #DISPLAYS NON COLOURBLIND MENU
+  puts "#{"*".light_blue}#{"*".cyan}#{"*".blue}"*17
+  puts "1. START GAME". center(51).light_blue
+  puts "2. EXIT".center(51).light_blue
+  puts "#{"*".light_blue}#{"*".cyan}#{"*".blue}"*17
+  input = gets().strip.upcase
+  if (input == "1")
+  #ENTER GAME START (NOT FINISHED)
+  elsif (input == "2")
+    return
+  else
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
+  end
+end
+  
+def colourblind_menu
+  #DISPLAYS COLOURBLIND MENU 
+  puts "*"*51
+  puts "1. START GAME".center(51)
+  puts "2. EXIT".center(51)
+  puts "*"*51
+  input = gets().strip.upcase
+  if (input == "1")
+    #ENTER GAME START (NOT FINISHED)
+  elsif (input == "2")
+    return
+  else
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+    puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+  end
+end
+  
 def quote_generator(total_quotes)
     quotes = []
     total_quotes.times do
@@ -17,39 +55,57 @@ def quote_generator(total_quotes)
     end
     return quotes
 end
-
-def intialize
+  
+  
+def colourblind_mode
     program_running = (true)
     while(program_running)
-      input = menu
-      if(input == 1)
-        #insert game loop here when made
-      elsif(input == 2)
-        break
+      input = launch_screen
+      if(input == "Y")
+        return true
+      
+      elsif(input == "N")
+        return false
+        
       else
-        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
-        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
-        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
-        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50).red
+        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
+        puts "DONT BREAK ME! PRESS 1 OR 2 PLEASE!".center(50)
       end
     end
 end
+  
+def program_launch
+  if colourblind_mode
+    colourblind_menu
+  else
+    main_menu
+  end
+end
+
+def quote_generator(total_quotes)
+  quotes = []
+  total_quotes.times do
+    quotes << Faker::GreekPhilosophers.quote
+  end
+    return quotes
+end
+
+quote_length = quotes[]
+
+def accuracy_percentage(errors, stringlength)
+acc_percentage = (error.to_f / stringlength.to_f) * 100
+return acc_percentage
+end
+#ACCURACY COUNTER
+
+
 
 # def game_loop  ### NOT COMPLETED ( USE THIS TO MOVE INTO GAME START)
 # end
 
 
-=======
-
-
-def quote_generator(n)
-    n.times do
-        puts Faker::GreekPhilosophers.quote
-    end
-end
-
-stage_1 = quote_generator(10)
->>>>>>> master
 
 #stage_1 generates the first stage of quotes ('Level 1')
 
