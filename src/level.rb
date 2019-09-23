8ca8176b0db24d0256a100472846753c079d85c3
Edward Deam <edwardcrdeam@gmail.com>
2019-09-23 20:52:11 +1000

    Refactor Level

--- src/level.rb
+++ src/level.rb
@@ -1,9 +1,10 @@
+# Level objects will store the quotes to be typed, as well as provide 
+# information about the total number of quotes and total num words that will
+# be used by other methods.
+
 require 'faker'
 
 class Level
-  # Level objects will store the quotes to be typed, as well as provide
-  # information about total num quotes and total num words that will be
-  # used by other methods.
   attr_reader :quotes
 
   def initialize(number_quotes)
@@ -17,21 +18,11 @@ class Level
 
   def total_words
     # Count the total number of words in all quotes in the array
-    words = 0
-    @quotes.each do |quote|
-      temp_array = quote.split
-      words += temp_array.length
-    end
-    return words
+    @quotes.flatten.split(' ').length
   end
 
-  def quote_generator(total_quotes)
-    quotes = []
-    total_quotes.times do
-      # Load random quotes from faker gem
-      quotes << Faker::GreekPhilosophers.quote
-    end
-      return quotes
+  def quote_generator(number_quotes)
+    Array.new(number_quotes) { Faker::GreekPhilosophers.quote }
   end
 
 end
